79b5a1bbdb6f83e52bc0f5f9b99585b2caac2519
vishal.patil@coxautoinc.com <vishalpatil550@gmail.com>
2023-07-13 15:01:35 +0530

    new page

---
+++ src/components/BuyProducts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const BuyProducts = (props: Props) => {
+  return (
+    <div style={{padding:50, backgroundColor:'whitesmoke'}}> <h1>Products: Comming Soon..</h1></div>
+  )
+}
+
+export default BuyProducts
