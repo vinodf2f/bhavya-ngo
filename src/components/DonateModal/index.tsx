6a178b9e9aec1b57ef39081551a641705203f477
Vinod <vinodyevatikar@Vinods-MacBook-Air.local>
2023-07-16 11:24:08 +0530

    Build error fix

--- src/components/DonateModal/index.tsx
+++ src/components/DonateModal/index.tsx
@@ -1,37 +1,44 @@
-import React from 'react';
+import React from "react";
 import "./index.css";
-import Scanner from '../../../public/scanner.jpeg';
-import Image from 'next/image'
+import Scanner from "../../../public/scanner.jpeg";
+import Image from "next/image";
 
 type Props = {
-  title: string,
-  content: string,
-  children: any,
-  closeModal: any
-}
-const DonateModal = ({ title, content, children, closeModal }: Props) => {
-  
+  title: string;
+  content: string;
+  closeModal: any;
+};
+const DonateModal = ({ title, content, closeModal }: Props) => {
   return (
     <div>
-        <div className="modal">
-          <div className="modal-content">
+      <div className="modal">
+        <div className="modal-content">
           <span className="close" onClick={closeModal}>
             &times;
           </span>
-            {title && <h2>{title}</h2>}
-            <hr></hr>
-            {content && <p className='content'>{content}</p>}
-            <div className='paymentDiv'>
-              <div className='paymentDiv-items'>
-                 <Image  src={Scanner} width={150} alt="Logo"/>
-              </div>
-              <div className='paymentDiv-items'>
-                <a ><h4 className='buyProducts'> CLICK HERE, You Can Donate School Kits or Buy Something From Portal Profit Will Be Used For Nobel Cause</h4></a>
-              </div>
+          {title && <h2>{title}</h2>}
+          <hr></hr>
+          {content && <p className="content">{content}</p>}
+          <div className="paymentDiv">
+            <div className="paymentDiv-items">
+              <Image src={Scanner} width={150} alt="Logo" />
             </div>
-            <h6>We are in the process of opening an account for our NGO. Until we open the account, we are accepting payments in a personal account.</h6>
+            <div className="paymentDiv-items">
+              <a>
+                <h4 className="buyProducts">
+                  {" "}
+                  CLICK HERE, You Can Donate School Kits or Buy Something From
+                  Portal Profit Will Be Used For Nobel Cause
+                </h4>
+              </a>
+            </div>
           </div>
+          <h6>
+            We are in the process of opening an account for our NGO. Until we
+            open the account, we are accepting payments in a personal account.
+          </h6>
         </div>
+      </div>
     </div>
   );
 };
